285346a26a7b3af2d1b10065b187362801e759c7
glutesha <mail@glutesha.me>
2025-06-27 22:07:15 +0300

    Design refresh...

--- src/lastfm.js
+++ src/lastfm.js
@@ -6,9 +6,9 @@ fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=glute
     const trackUrl = track.url;
     const trackImage = track.image[3]["#text"];
     const trackHtml = `
-        <div class="flex flex-col md:flex-row relative ">
+        <div class="flex flex-col md:flex-row relative font-sans font-stretch-ultra-expanded">
             <img class="rounded-3xl aspect-square shrink-0 z-1" src="${trackImage}" alt="${trackName}">
-            <div class="flex justify-center px-1 md:px-5 py-2 flex-col text-2xl font-mono font-bold gap-2">
+            <div class="flex justify-center px-1 md:px-5 py-2 flex-col text-2xl font-bold gap-2">
                 <img alt="chuvirlatribal" class="absolute -right-20 -bottom-28 max-h-70 md:max-h-max md:left-auto md:right-3 flex-1 md:max-w-md md:-top-4 invert opacity-10" src="${tribal}">
                 <p class="pt-2 text-4xl md:text-5xl z-1">${trackName}</p>
                 <p class="text-xl z-1">${artistName}</p>
--- src/webring.js
+++ src/webring.js
@@ -6,3 +6,15 @@ document.getElementById('prevurl').href = prev.url;
 document.getElementById('nexturl').innerText = next.name + " >";
 document.getElementById('nexturl').href = next.url;
 });
+window.addEventListener('scroll', function () {
+    const webring = document.getElementById('webring');
+    const scrolled = window.scrollY > 20;
+
+    if (scrolled) {
+        webring.classList.remove('translate-y-50');
+        webring.classList.add('translate-y-0');
+    } else {
+        webring.classList.remove('translate-y-0');
+        webring.classList.add('translate-y-50');
+    }
+});
