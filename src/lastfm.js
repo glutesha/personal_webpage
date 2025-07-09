27787fa1e9414c9e8587714808d63f84609ff21d
glutesha <mail@glutesha.me>
2025-07-09 16:55:45 +0300

    Lastfm back color in album cover colors!

--- src/lastfm.js
+++ src/lastfm.js
@@ -1,5 +1,16 @@
 import tribal from '../imgs/tribal.png';
-const ColorThief = require('colorthief');
+import ColorThief from 'colorthief';
+const colorThief = new ColorThief();
+
+//https://awik.io/determine-color-bright-dark-using-javascript/
+function getBrightness([r, g, b]) {
+  return Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b);
+}
+
+function adjustFactor(brightness) {
+  const factor = brightness > 127.5 ? 0.2 : 0.5;
+  return factor;
+}
 
 fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=glutesha&api_key=ee9f8ab4cf3c744ba03248f2ac1f19e8&format=json").then(r => r.json()).then(r => r.recenttracks.track[0]).then(track => {
     const trackName = track.name;
@@ -7,6 +18,27 @@ fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=glute
     const albumName = track.album["#text"];
     const trackUrl = track.url;
     const trackImage = track.image[3]["#text"];
+
+    const img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.src = trackImage;
+    
+    const trackContainer = document.getElementById('lastfm');
+    
+    img.onload = () => {
+        const colorThief = new ColorThief();
+        const [r, g, b] = colorThief.getColor(img);
+        const brightness = getBrightness([r, g, b]);
+        const factor = adjustFactor(brightness);
+
+        trackContainer.addEventListener('mouseenter', () => {
+        trackContainer.style.boxShadow = `0 20px 25px rgba(${r},${g},${b},${factor})`;
+        });
+        trackContainer.addEventListener('mouseleave', () => {
+        trackContainer.style.boxShadow = '';
+        });
+    };
+
     const trackHtml = `
         <div class="flex flex-col md:flex-row relative font-mona-sans font-black font-stretch-125%">
             <img class="rounded-3xl aspect-square shrink-0 z-1" src="${trackImage}" alt="${trackName}">
@@ -20,6 +52,6 @@ fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=glute
         </div>
     `;
 
-    const trackContainer = document.getElementById('lastfm');
+    
     trackContainer.innerHTML = trackHtml;
 });
